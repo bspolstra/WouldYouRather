283c26e6c743e631fd61982af0bbff70dd2a59c4
Benjamin Polstra <bspolstra@gmail.com>
2019-11-02 07:56:23 -0700

    Changed configuration relevant to saving answer

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,42 @@
-import { SET_QUESTIONS } from "../util/constants";
+import { SET_QUESTIONS, ADD_QUESTION, SAVE_ANSWER } from "../util/constants";
+import { _saveQuestion, _saveQuestionAnswer } from "../util/_DATA";
 
 export function setQuestions(questions) {
   return {
     type: SET_QUESTIONS,
     questions
+  };
+}
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  };
+}
+
+function saveAnswer(question) {
+  const { qid, answer, authedUser } = question;
+  return {
+    type: SAVE_ANSWER,
+    qid,
+    answer,
+    authedUser
+  };
+}
+
+export function handleCreateQuestion(optionOneText, optionTwoText, author) {
+  return dispatch => {
+    _saveQuestion({ optionOneText, optionTwoText, author }).then(question =>
+      dispatch(addQuestion(question))
+    );
+  };
+}
+
+export function handleSaveAnswer(authedUser, qid, answer) {
+  return dispatch => {
+    _saveQuestionAnswer({ authedUser, qid, answer }).then(() =>
+      dispatch(saveAnswer({ authedUser, qid, answer }))
+    );
   };
 }
