5b684f9ad6762680ad5e873730a659d3c60b653a
Benjamin Polstra <bspolstra@gmail.com>
2019-11-03 12:50:55 -0800

    Established the styling links

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "./bootstrap.min.css";
 
 const store = createStore(reducer, middleware);
 
